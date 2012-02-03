43141de452b5021b50ae716eac4c23c494de8493
Mocel <docel77@gmail.com>
2012-02-03 16:18:46 +0900

    StringBuffer を使わないようにした。

--- twitter4j-core/src/main/java/twitter4j/internal/org/json/JSONObject.java
+++ twitter4j-core/src/main/java/twitter4j/internal/org/json/JSONObject.java
@@ -339,7 +339,7 @@ public class JSONObject {
             Object key = keys.nextElement();
             if (key instanceof String) {
 
-// Go through the path, ensuring that there is a nested JSONObject for each 
+// Go through the path, ensuring that there is a nested JSONObject for each
 // segment except the last. Add the value using the last segment's name into
 // the deepest nested JSONObject.
 
@@ -962,7 +962,7 @@ public class JSONObject {
     private void populateMap(Object bean) {
         Class klass = bean.getClass();
 
-// If klass is a System class then set includeSuperClass to false. 
+// If klass is a System class then set includeSuperClass to false.
 
         boolean includeSuperClass = klass.getClassLoader() != null;
 
@@ -1267,8 +1267,8 @@ public class JSONObject {
         }
 
         /*
-         * If it might be a number, try converting it. 
-         * We support the non-standard 0x- convention. 
+         * If it might be a number, try converting it.
+         * We support the non-standard 0x- convention.
          * If a number cannot be produced, then the value will just
          * be a string. Note that the 0x-, plus, and implied string
          * conventions are non-standard. A JSON parser may accept
